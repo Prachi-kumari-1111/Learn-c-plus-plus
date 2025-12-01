8f34c766edf42a264830be020ea237f32de24a0f
Prachi-kumari-1111 <prachikri8840@gmail.com>
2025-12-01 21:26:29 +0530

    e:\Lets Learn C++\Learn-c-plus-plus\Revise\age.cpp

---
+++ Revise/age.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+  int age=18;
+  cout << "enter the age" << endl;
+  cin>> age;
+  
+
+if(age > 150){
+
+    cout<< "Invalid age";
+}
+
+ else if(age >= 18){
+     
+    cout<< "you can vote";
+ }
+
+ else   
+ {
+    cout<< "you cannot vote";
+ }
+ 
+}
